7074c1b1f90224cb2aefc421b738f3bd60ff5e5e
Joseph L <vipercar233@gmail.com>
2022-07-29 01:13:41 -0500

    created random var to get length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
 // Assignment code here
 
+// ask for number of length
+var getPasswordLength = function() {
+  
+  var length = window.prompt("Pick a number of characters between 8-128");
+
+  if (length <=128 && length >= 8) {
+      console.log("The number of character is " + length); 
+    } else {
+    window.alert("Please enter the number between 8-128.");
+    return getPasswordLength();
+  }
+};
+getPasswordLength();
+
+// create a variable of characters 
+// var chars = "0123456789abcedfghijklmnopqrstuvwxyz!@#$%^&*()ABCEDFGHIJKLMNOPQRSTUVWXYZ"
+
+
+// create a random password loop with Math.random()
+// for (var i = 0; i <= passwordLength; i++)
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
